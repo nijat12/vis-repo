6050b7b279dc726fc8e76474dc0b3b1f0eb659ae
Nijat Zeynalov <nijat12@gmail.com>
2025-12-28 18:55:08 +0000

    Startup bug fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-git
+git libgl1-mesa-glx libglib2.0-0
 
 # 2. Establish SSH Key
 if [ ! -f ~/.ssh/id_ed25519 ]; then
